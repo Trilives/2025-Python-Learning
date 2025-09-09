bb1832c39d8ea34fb4ce2924ac7924cdfcef10b8
Trilives <Trlives@outlook.com>
2025-09-09 11:44:44 +0800

    25.9.9

---
+++ 01/2.整数.py
@@ -0,0 +1,11 @@
+#整数定义不需要预约格式
+x = 1 + 2
+y = 1 - 2
+z = 1 * 2
+h = 1 / 2
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(h))
+#自动切换了数据的格式
+#其他运算符，x ** y x的y次幂，x // y 返回商的整数部分
---
+++ 01/4.布尔.py
@@ -0,0 +1,7 @@
+#基本数据类型，False和True
+#逻辑运算and,or和not
+a = True
+b = False
+print(a and b)
+print(a or b)
+print(not a)
